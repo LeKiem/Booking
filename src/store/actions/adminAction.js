3d045478770ccdded66b67f9e1f259e5a4d9525c
LeKiem <levankiemdev@gmail.com>
2023-10-06 21:15:48 +0700

    #59 Debugs Redux Từ A đến Z và Thực Hiện CRUD Với Redux - CREATE

--- src/store/actions/adminAction.js
+++ src/store/actions/adminAction.js
@@ -1,5 +1,8 @@
 import actionTypes from "./actionTypes";
-import { getAllCodeService } from "../../services/userService";
+import {
+  getAllCodeService,
+  createNewUserService
+} from "../../services/userService";
 
 // export const fetchGenderStart = () => ({
 //   type: actionTypes.FETCH_GENDER_START
@@ -78,3 +81,28 @@ export const fetchRoleStart = () => {
   };
 };
 // export default adminActions;
+export const createNewUser = data => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await createNewUserService(data);
+      // console.log("hhhhh", res);
+      if (res && res.errCode === 0) {
+        // toast.success("Create a new user Success");
+        dispatch(saveUserSuccess());
+        // dispatch(fetchAllUsersStart());
+      } else {
+        dispatch(saveUserFailed());
+      }
+    } catch (e) {
+      dispatch(saveUserFailed());
+      console.log(e);
+    }
+  };
+};
+export const saveUserSuccess = () => ({
+  type: actionTypes.CREATE_USER_SUCCESS
+});
+
+export const saveUserFailed = () => ({
+  type: actionTypes.CREATE_USER_FAILED
+});
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -41,6 +41,12 @@ const adminReducer = (state = initialState, action) => {
     case actionTypes.FETCH_ROLE_FAILED:
       state.roles = [];
       return { ...state };
+    case actionTypes.FETCH_ALL_USERS_SUCCESS:
+      state.users = action.users;
+      return { ...state };
+    case actionTypes.FETCH_ALL_USERS_FAILED:
+      state.users = [];
+      return { ...state };
     default:
       return state;
   }
