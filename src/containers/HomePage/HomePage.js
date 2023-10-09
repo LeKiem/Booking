33c10b85e9d34de52ba0a1c594fcd0525dfcacbf
LeKiem <levankiemdev@gmail.com>
2023-10-09 08:09:03 +0700

    #64 Tạo Data Hoàn Tất Section Bác Sĩ Nổi Bật Trong Tuần

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -12,13 +12,17 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 class HomePage extends Component {
+  // handleAfterChange= (event, slick ,currentSlide) =>{
+
+  // }
   render() {
     let settings = {
       dots: false,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 4,
       slidesToScroll: 1
+      // slickGoTo: this.handleAfterChange
     };
     return (
       <div>
--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import Slider from "react-slick";
 import * as actions from "../../../store/actions";
 import { LANGUAGES } from "../../../utils";
+import { FormattedMessage } from "react-intl";
 
 class OutStandingDoctor extends Component {
   constructor(props) {
@@ -31,8 +32,12 @@ class OutStandingDoctor extends Component {
       <div className="section-share section-outstanding-doctor">
         <div className="section-container">
           <div className="section-header">
-            <span className="title-section">Bác sĩ nổi bật tuần qua</span>
-            <button className="btn-section">Xem thêm</button>
+            <span className="title-section">
+              <FormattedMessage id={"homepage.outstanding-doctor"} />
+            </span>
+            <button className="btn-section">
+              <FormattedMessage id={"homepage.more-info"} />
+            </button>
           </div>
           <div className="section-body">
             <Slider {...this.props.settings}>
@@ -55,7 +60,9 @@ class OutStandingDoctor extends Component {
                         <div className="outer-bg">
                           <div
                             className="bg-img section-outstanding-doctor"
-                            style={{ backgroundImage: `url(${imageBase64})` }}
+                            style={{
+                              backgroundImage: `url(${imageBase64})`
+                            }}
                           />
                         </div>
                         <div className="position text-center">
